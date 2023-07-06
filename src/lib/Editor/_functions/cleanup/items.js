531041775422329ecc784cf63900a9ddd5454ef4
Steven Bell <dev.thebells1111@gmail.com>
2023-07-05 23:53:11 -0500

    working on fixing vts

--- src/lib/Editor/_functions/cleanup/items.js
+++ src/lib/Editor/_functions/cleanup/items.js
@@ -2,7 +2,6 @@ import cleanPodcastImages from './images';
 import cleanEpisodePerson from './episodePerson';
 import cleanPodcastSocialInteract from './socialInteract';
 import cleanLicense from './license';
-import cleanValueAudioItem from './splitValueBlock';
 import cleanChat from '$lib/Editor/Tags/Chat/cleanChat';
 import cleanLiveValue from '$lib/Editor/Tags/LiveValue/cleanLiveValue';
 import { get } from 'svelte/store';
@@ -52,7 +51,6 @@ async function cleanItem(item, data) {
 	cleanEpisodeTranscript(item);
 	cleanPodcastImages(item);
 	cleanLicense(item);
-	cleanValueAudioItem(item, data);
 	cleanChat(item);
 	cleanLiveValue(item);
 	// console.log(item.description);
@@ -119,6 +117,71 @@ async function cleanItem(item, data) {
 			delete item.guid['@_isPermalink'];
 		}
 	}
+
+	item['podcast:value'] = item['podcast:value'] || {};
+	delete item['podcast:value']['podcast:valueTimeSplits'];
+
+	if (item?.valueTimeSplit) {
+		console.log(item?.valueTimeSplit);
+
+		let vts = item?.valueTimeSplit;
+		if (vts) {
+			console.log(vts);
+			let splits = []
+				.concat(vts)
+				.map((v) => {
+					console.log(v);
+					let split = {};
+					if (v['@_startTime']) {
+						split['@_startTime'] = v['@_startTime'];
+					} else {
+						return;
+					}
+					if (v['@_duration']) {
+						split['@_duration'] = v['@_duration'];
+					} else {
+						return;
+					}
+					split['@_remotePercentage'] = v['@_remotePercentage'] || 100;
+
+					if (v['@_feedGuid'] || v?.['podcast:valueRecipient']?.some((v) => v['@_address'])) {
+						split['podcast:remoteItem'] = {};
+						if (v['@_feedGuid']) {
+							console.log(v);
+							split['podcast:remoteItem']['@_feedGuid'] = v['@_feedGuid'];
+							if (v['@_itemGuid']) {
+								split['podcast:remoteItem']['@_itemGuid'] = v['@_itemGuid'];
+							}
+						} else {
+							console.log(v);
+							split['podcast:valueRecipient'] = v['podcast:valueRecipient']
+								.map((v) => {
+									if (v['@_address']) {
+										return v;
+									}
+								})
+								.filter((v) => v);
+						}
+
+						return v;
+					}
+				})
+				.filter((v) => {
+					console.log(v);
+					return v;
+				});
+
+			if (splits?.length) {
+				console.log(splits);
+
+				item['podcast:value']['podcast:valueTimeSplits'] = splits;
+			} else {
+				delete item['podcast:value']['podcast:valueTimeSplits'];
+			}
+		}
+
+		delete item?.valueTimeSplit;
+	}
 }
 
 async function handleTrackers(item) {
@@ -179,11 +242,8 @@ async function cleanLiveItem(item) {
 		item['@_status'] = 'pending';
 	}
 
-	console.log(item);
-	console.log(item?.['podcast:value']?.['podcast:valueTimeSplit']);
 	delete item?.['podcast:value']?.['podcast:valueTimeSplit'];
-	console.log(item);
-	console.log(item?.['podcast:value']?.['podcast:valueTimeSplit']);
+	delete item.valueTimeSplit;
 }
 
 function processLiveItemTimes(item) {
