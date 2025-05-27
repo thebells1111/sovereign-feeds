dc14cf7f4360d1c3d012c5441551e3b2721b64f5
Steven Bell <dev.thebells1111@gmail.com>
2025-05-27 11:43:16 -0500

    fixed parsing error when location text only

--- src/lib/Tags/Location/initializeLocation.js
+++ src/lib/Tags/Location/initializeLocation.js
@@ -4,6 +4,12 @@ import clone from 'just-clone';
 export default function initializeLocationTag(data) {
 	if (!data) return [clone(blankLocation)];
 
+	if (typeof data === 'string') {
+		const loc = clone(blankLocation);
+		loc['#text'] = data;
+		return [loc];
+	}
+
 	const cloned = clone(data);
 	return Array.isArray(cloned) ? cloned : [cloned];
 }
